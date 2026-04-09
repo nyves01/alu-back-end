ed583d56a3bd4e1287521c5b02554e2161ddbf92
nyves01 <n.yves@alustudent.com>
2026-04-09 22:54:22 +0000

    Refactor main function to simplify user ID handling and improve error messaging

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -30,14 +30,9 @@ def get_user_todos(user_id):
 def main():
     """Program entry point."""
     if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]), file=sys.stderr)
         sys.exit(1)
 
-    try:
-        user_id = int(sys.argv[1])
-    except ValueError:
-        print("employee_id must be an integer", file=sys.stderr)
-        sys.exit(1)
+    user_id = sys.argv[1]
 
     user = get_user(user_id)
     todos = get_user_todos(user_id)
@@ -52,6 +47,7 @@ def main():
             len(todos)
         )
     )
+
     for task in completed_tasks:
         print("\t {}".format(task.get("title")))
 
